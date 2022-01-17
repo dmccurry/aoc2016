c53ec77cf7048a0d56d0a5aac22f4de9f187e42e
David McCurry <dmccurry@gmail.com>
2022-01-17 16:08:23 -0400

    Removing target

--- day01/target/debug/day01.d
+++
@@ -1,1 +0,0 @@
-/home/dave/projects/aoc2016/day01/target/debug/day01: /home/dave/projects/aoc2016/day01/src/main.rs
--- day01/target/debug/deps/day01-eb3448b792f09cce.d
+++
@@ -1,5 +0,0 @@
-/home/dave/projects/aoc2016/day01/target/debug/deps/day01-eb3448b792f09cce: src/main.rs
-
-/home/dave/projects/aoc2016/day01/target/debug/deps/day01-eb3448b792f09cce.d: src/main.rs
-
-src/main.rs:
